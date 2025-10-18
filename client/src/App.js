4be63c71005927924d8a8bebaccd8301842adf98
Ahmed Ibrahim <ahmedgouda797@gmail.com>
2025-10-18 15:10:55 -0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>        
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
